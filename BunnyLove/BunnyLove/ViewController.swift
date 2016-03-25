035d85ff268d47b8921ff9fe5395c14f81fc583c
Shane Zatezalo <shane@lottadot.com>
2016-03-24 22:09:58 -0400

    Add initial ViewController class with ivar/Outlets

---
+++ BunnyLove/BunnyLove/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  BunnyLove
+//
+//  Created by Shane Zatezalo on 3/24/16.
+//  Copyright © 2016 Lottadot LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var bunnyImageView: UIImageView!
+    @IBOutlet weak var cuteButton: UIButton!
+    @IBOutlet weak var uglyButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
