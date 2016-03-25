23187638162290d8fc2936b5db24eccb582b66fa
Shane Zatezalo <shane@lottadot.com>
2016-03-24 22:09:30 -0400

    Add initial class stubs for provider and keeper

---
+++ BunnyLove/BunnyLove/BunnyProvider.swift
@@ -0,0 +1,14 @@
+//
+//  BunnyProvider.swift
+//  BunnyLove
+//
+//  Created by Shane Zatezalo on 3/24/16.
+//  Copyright © 2016 Lottadot LLC. All rights reserved.
+//
+
+import Foundation
+
+/// This class will provide Bunnies.
+class BunnyProvider {
+    
+}
---
+++ BunnyLove/BunnyLove/ScoreKeeper.swift
@@ -0,0 +1,14 @@
+//
+//  ScoreKeeper.swift
+//  BunnyLove
+//
+//  Created by Shane Zatezalo on 3/24/16.
+//  Copyright © 2016 Lottadot LLC. All rights reserved.
+//
+
+import Foundation
+
+/// This class will manage the score keeping.p
+class ScoreKeeper {
+    
+}
