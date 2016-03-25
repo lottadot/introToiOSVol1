68588685dc30143062d1e80b978ea2cc5f92390f
Shane Zatezalo <shane@lottadot.com>
2016-03-25 08:00:36 -0400

    Add UIAcceptance Tests

--- BunnyLove/BunnyLoveUITests/BunnyLoveUITests.swift
+++ BunnyLove/BunnyLoveUITests/BunnyLoveUITests.swift
@@ -29,8 +29,21 @@ class BunnyLoveUITests: XCTestCase {
     }
     
     func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        let app = XCUIApplication()
+        let cuteButton = app.buttons["Cute"]
+        cuteButton.tap()
+        
+        let mehButton = app.buttons["Meh"]
+        mehButton.tap()
+        cuteButton.tap()
+        cuteButton.tap()
+        cuteButton.tap()
+        cuteButton.tap()
+        mehButton.tap()
+        mehButton.tap()
+        app.alerts["Results"].collectionViews.buttons["Results"].tap()
+        
     }
     
 }
