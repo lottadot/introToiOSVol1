4f2bd32ddbd5e7a3d6412169566cbc2a0d98ebc7
Shane Zatezalo <shane@lottadot.com>
2016-03-25 07:59:33 -0400

    Add some logical tests

--- BunnyLove/BunnyLoveTests/BunnyLoveTests.swift
+++ BunnyLove/BunnyLoveTests/BunnyLoveTests.swift
@@ -11,26 +11,16 @@ import XCTest
 
 class BunnyLoveTests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func test01BunnyProviderHasBunnies() {
+        let bunnyProvider = BunnyProvider.init()
+        XCTAssertTrue(bunnyProvider.bunnies.count > 0)
     }
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
+    func test0FirstBunny() {
+        let bunnyProvider = BunnyProvider.init()
+        
+        let bunny1 = bunnyProvider.bunnies.first
+        XCTAssertEqual(bunny1?.filename, "Bunny1")
     }
     
 }
